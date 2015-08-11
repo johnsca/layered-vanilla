8b2889e97de72fbc6ff5365a7c08a672797f7fbd
Cory Johns <johnsca@gmail.com>
2015-08-11 18:20:27 -0400

    First pass at layered vanilla

---
+++ reactive/vanilla.py
@@ -0,0 +1,16 @@
+from charmhelpers.core.templating import render
+from charms.reactive import when
+from charms.reactive import set_state
+
+
+@when('apache.project.available', 'mysql.available')
+def setup_vanilla(mysql):
+    render(source='vanilla_config.php',
+           target='/var/www/vanilla/conf/config.php',
+           context={
+               'db_host': mysql.host(),
+               'db_database': mysql.database(),
+               'db_user': mysql.user(),
+               'db_password': mysql.password(),
+           })
+    set_state('apache.start')
---
+++ templates/vanilla_config.php
@@ -0,0 +1,6 @@
+<?php if (!defined('APPLICATION')) exit();
+$Configuration['Database']['Host'] = '{{db_host}}';
+$Configuration['Database']['Name'] = '{{db_db}}';
+$Configuration['Database']['User'] = '{{db_user}}';
+$Configuration['Database']['Password'] = '{{db_pass}}';
+?>
