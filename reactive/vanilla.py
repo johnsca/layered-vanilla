f5b8ac95e2fc25ffb4fada8eb0bba8408ffb35cd
Cory Johns <johnsca@gmail.com>
2015-08-19 12:29:14 -0400

    Fixes from testing and improved status reporting

--- reactive/vanilla.py
+++ reactive/vanilla.py
@@ -1,16 +1,41 @@
+import pwd
+import os
+from charmhelpers.core.hookenv import status_set
 from charmhelpers.core.templating import render
-from charms.reactive import when
-from charms.reactive import set_state
+from charms.reactive import when, when_not
+from charms.reactive import set_state, remove_state
 
 
-@when('apache.available', 'mysql.available')
+@when('apache.available', 'database.available')
 def setup_vanilla(mysql):
     render(source='vanilla_config.php',
            target='/var/www/vanilla/conf/config.php',
+           owner='www-data',
+           perms=0o775,
            context={
-               'db_host': mysql.host(),
-               'db_database': mysql.database(),
-               'db_user': mysql.user(),
-               'db_password': mysql.password(),
+               'db': mysql,
            })
+    uid = pwd.getpwnam('www-data').pw_uid
+    os.chown('/var/www/vanilla/cache', uid, -1)
+    os.chown('/var/www/vanilla/uploads', uid, -1)
     set_state('apache.start')
+    status_set('maintenance', 'Starting Apache')
+
+
+@when('apache.available')
+@when_not('database.connected')
+def missing_mysql():
+    remove_state('apache.start')
+    status_set('blocked', 'Please add relation to MySQL')
+
+
+@when('database.connected')
+@when_not('database.available')
+def waiting_mysql(mysql):
+    remove_state('apache.start')
+    status_set('waiting', 'Waiting for MySQL')
+
+
+@when('apache.started')
+def started():
+    status_set('active', 'Ready')
--- templates/vanilla_config.php
+++ templates/vanilla_config.php
@@ -1,6 +1,6 @@
 <?php if (!defined('APPLICATION')) exit();
-$Configuration['Database']['Host'] = '{{db_host}}';
-$Configuration['Database']['Name'] = '{{db_db}}';
-$Configuration['Database']['User'] = '{{db_user}}';
-$Configuration['Database']['Password'] = '{{db_pass}}';
+$Configuration['Database']['Host'] = '{{ db.host() }}';
+$Configuration['Database']['Name'] = '{{ db.database() }}';
+$Configuration['Database']['User'] = '{{ db.user() }}';
+$Configuration['Database']['Password'] = '{{ db.password() }}';
 ?>
